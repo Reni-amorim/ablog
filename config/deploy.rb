5b0a080cfaee776a011960237c3494dabf5783c4
Reni-amorim <reni.425@gmail.com>
2023-10-18 16:26:55 -0300

    set new prefix

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,8 @@ server '15.229.7.177', port: 22, roles: [:web, :app, :db], primary: true
 set :rbenv_type, :system
 set :rbenv_ruby,      '3.0.6'
 set :default_env, { 'RBENV_ROOT' => "$HOME/.rbenv" }
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} RBENV_ROOT=#{fetch(:rbenv_path)}"
+#set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 ##################### try
 #set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
